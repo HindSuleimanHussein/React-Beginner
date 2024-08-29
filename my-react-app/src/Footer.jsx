2e91bec82bc7ab2ef4991c08581cb6de8be06675
Hind Hussein <hind.suleimanh@gmail.com>
2024-08-29 22:11:38 +0300

    Footer

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} https://github.com/HindSuleimanHussein?tab=repositories</p>
+        </footer>
+    );
+}
+
+export default Footer
