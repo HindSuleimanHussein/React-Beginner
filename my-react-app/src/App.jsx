bdf6eb43be65dbad8504ef9b97908b63feb91102
Hind Hussein <hind.suleimanh@gmail.com>
2024-08-29 22:27:14 +0300

    Profile Dashboard

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,15 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import Experience from './Experience.jsx'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Experience/>
       <Footer/>
+      
     </>
   );
 }
---
+++ my-react-app/src/Experience.jsx
@@ -0,0 +1,17 @@
+
+function Experience() {
+    const firstExperience = "Web Developer";
+    const secondExperience = "Mobile App Developer";
+    return (
+        <>
+            <h2>Experience</h2>
+
+            <ul>
+                <li>{firstExperience}</li>
+                <li>{secondExperience}</li>
+            </ul>
+        </>
+    );
+}
+
+export default Experience
