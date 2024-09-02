d41441c6576a0cb30a3448c428a477455be23dd9
Hind Hussein <hind.suleimanh@gmail.com>
2024-09-03 00:39:40 +0300

    CSS and Props

--- css-props/src/App.jsx
+++ css-props/src/App.jsx
@@ -1,6 +1,15 @@
-
+import Student from './Student.jsx'
+//http://localhost:5175/
 
 function App() {
+  return (
+    <>
+      <Student name="Hind" age="21" isStudent="false" />
+      <Student name="Nada" age="21" isStudent="true" />
+      <Student name="Ghaid" age="22" isStudent="true" />
+      <Student age="21"/>
+    </>
+  );
 }
 
 export default App
