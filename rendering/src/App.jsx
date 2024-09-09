98da6937aafa4aab6ad8c7ceaf15923682aa0b36
Hind Hussein <hind.suleimanh@gmail.com>
2024-09-09 21:56:43 +0300

    rendering

--- rendering/src/App.jsx
+++ rendering/src/App.jsx
@@ -1,8 +1,14 @@
 
 //http://localhost:5173/
 
+import UserGreeting from "./UserGreeting"
+
 function App() {
- 
+ return(
+  <>
+  <UserGreeting isLoggedIn={true} username="Hind"/>
+  </>
+ )
 }
 
 export default App
---
+++ rendering/src/UserGreeting.jsx
@@ -0,0 +1,12 @@
+
+function UserGreeting(props){
+
+    if(props.isLoggedIn){
+        return <h2> Welcome {props.username}</h2>
+    } 
+    else {
+        return <h2>Please Log in to Continue</h2>
+    }
+}
+
+export default UserGreeting
